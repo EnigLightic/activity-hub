d2154fad477fc62d62b761755752437a413c464d
EnigLightic <m19967821793@163.com>
2026-03-01 17:09:56 +0800

    添加JWT黑名单机制

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -1,18 +1,27 @@
 from flask import Flask
-from .extensions import db,migrate
-
+from .extensions import db, migrate, jwt
+from app.models.token_blocklist import TokenBlocklist
 
 def create_app():
     app = Flask(__name__)
     app.config.from_object("config.Config")
 
     db.init_app(app)
-    migrate.init_app(app,db)
+    migrate.init_app(app, db)
+    jwt.init_app(app)
 
     from .models import User
     from .routes.main import main_bp
     app.register_blueprint(main_bp)
+
     from app.routes.auth import auth_bp
     app.register_blueprint(auth_bp)
 
     return app
+
+
+@jwt.token_in_blocklist_loader
+def check_if_token_revoked(jwt_header, jwt_payload):
+    jti = jwt_payload["jti"]
+    token = TokenBlocklist.query.filter_by(jti=jti).first()
+    return token is not None
